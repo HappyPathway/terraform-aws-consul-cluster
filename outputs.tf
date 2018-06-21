10fcf5bd5389caefac5e8be8a84fc53d11748edc
Dave Arnold <darnold@hashicorp.com>
2018-06-20 21:23:52 -0700

    adding public address and changing private address

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "server_addresses" {
+output "public_addresses" {
+  value = "${aws_instance.server.*.public_ip}"
+}
+
+output "private_addresses" {
   value = "${aws_instance.server.*.private_ip}"
 }
